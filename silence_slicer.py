67e4c91cccd5de27a4b8aa22023222c8d641cac3
ckdgus0505 <ckdgus0505@gmail.com>
2020-07-08 17:27:38 +0900

    Update silence_slicer.py

--- silence_slicer.py
+++ silence_slicer.py
@@ -17,15 +17,20 @@ def silence_slicer(path_in, path_out, format="wav"):
         non_sil_times = [t for t in non_sil_times_concat if t[1] - t[0] > 350]
         sound[non_sil_times[0][0]: non_sil_times[-1][1]].export(path_out)
 
+
 relative_path = './data/'
 file_path = 'VCTK-Corpus/wav48/'
-
+f = open('error.txt', mode = 'w', encoding = 'utf-8')
 for subdir, dirs, files in os.walk(relative_path+file_path):
-
     for wav_file in files:
         tmp = subdir.split('/')[-1]
         save_path = './sliced_data/VCTK_Corpus/wav48/'+tmp+'/'
-        print(save_path)
         os.makedirs(save_path, exist_ok=True)
-        silence_slicer(subdir+'/'+wav_file, save_path+wav_file)
+        try:
+            silence_slicer(subdir+'/'+wav_file, save_path+wav_file)
+        except Exception as ex:
+            copyfile(subdir+'/'+wav_file, save_path+wav_file)
+
+            f.write(wav_file + '\n')
+f.close()
 
